f225f1f8e5db091772051da98e3105f5f8a25e1c
Juan Pariente <jpariente000@gmail.com>
2026-05-22 17:07:16 -0300

    Remove try-catch block

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,28 +1,14 @@
 import db from './db.js'
 
-// const getAllOrganizations = async() => {
-//     const query = `
-//         SELECT organization_id, name, description, contact_email, logo_filename
-//       FROM public.organization;
-//     `;
-
-//     const result = await db.query(query);
-
-//     return result.rows;
-// }
-
-const getAllOrganizations = async () => {
-  try {
-    const result = await db.query(`
-      SELECT organization_id, name, description, contact_email, logo_filename
+const getAllOrganizations = async() => {
+    const query = `
+        SELECT organization_id, name, description, contact_email, logo_filename
       FROM public.organization;
-    `);
+    `;
+
+    const result = await db.query(query);
 
     return result.rows;
-  } catch (err) {
-    console.error("🔥 DB ERROR:", err);
-    throw err;
-  }
-};
+}
 
-export {getAllOrganizations} 
+export {getAllOrganizations}
