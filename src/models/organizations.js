84d8ee39c2bd86b402e50d5170dfae3ca2ba1a89
Juan Pariente <jpariente000@gmail.com>
2026-05-22 16:53:52 -0300

    add a try block

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,14 +1,28 @@
 import db from './db.js'
 
-const getAllOrganizations = async() => {
-    const query = `
-        SELECT organization_id, name, description, contact_email, logo_filename
-      FROM public.organization;
-    `;
+// const getAllOrganizations = async() => {
+//     const query = `
+//         SELECT organization_id, name, description, contact_email, logo_filename
+//       FROM public.organization;
+//     `;
 
-    const result = await db.query(query);
+//     const result = await db.query(query);
+
+//     return result.rows;
+// }
+
+const getAllOrganizations = async () => {
+  try {
+    const result = await db.query(`
+      SELECT organization_id, name, description, contact_email, logo_filename
+      FROM public.organization;
+    `);
 
     return result.rows;
-}
+  } catch (err) {
+    console.error("🔥 DB ERROR:", err);
+    throw err;
+  }
+};
 
 export {getAllOrganizations} 
