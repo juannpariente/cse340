41c622bd8e6cde5fbfabe1d402d0ae9b188b3d90
Juan Pariente <jpariente000@gmail.com>
2026-05-22 16:32:29 -0300

    display database results in EJS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -36,8 +37,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+  const organizations = await getAllOrganizations();
+
   const title = 'Our Partner Organizations';
-  res.render('organizations', { title });
+  res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
