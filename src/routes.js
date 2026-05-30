6574cf4c7f211c8160dd014b4780310cc0b5be6f
Juan Pariente <jpariente000@gmail.com>
2026-05-30 17:24:21 -0300

    Add project details page

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import { showProjectsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 import { showOrganizationDetailsPage } from './controllers/organizations.js';
+import { showProjectDetailsPage } from './controllers/projects.js';
 
 const router = express.Router();
 
@@ -15,6 +16,8 @@ router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
 // Route for organization details page
 router.get('/organization/:id', showOrganizationDetailsPage);
+// Route for project details page
+router.get('/project/:id', showProjectDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
