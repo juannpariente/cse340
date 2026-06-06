57a8d15c7b97715f5d368a3379363af676b78537
Juan Pariente <jpariente000@gmail.com>
2026-06-06 07:34:44 -0300

    Add learning week 4

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
+import session from 'express-session';
+import flash from './src/middleware/flash.js';
+
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -20,6 +24,17 @@ const app = express();
   */
 
 app.use("/bootstrap", express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
 
 // Allow Express to receive and process common POST data
 app.use(express.urlencoded({ extended: true }));
